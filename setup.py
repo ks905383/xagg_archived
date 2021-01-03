89dd432ed0fcfd477f9778fbff947253d9e1cd39
Kevin Schwarzwald <ks905383@gmail.com>
2021-01-03 16:25:48 -0500

    prep for creating package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xagg",
+    version="0.0.1",
+    author="Kevin Schwarzwald",
+    author_email="kschwarzwald@iri.columbia.edu",
+    description="Aggregating raster data over polygons",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ks905383/xagg",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU GPLv3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
